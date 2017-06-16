a328f88f6127963d201c46ca40665cc3648d1671
gato-cosmico <elgatocosmico66@gmail.com>
2017-06-15 19:49:32 -0500

    inicio a aprender ruby

---
+++ hola_mundo.rb
@@ -0,0 +1,10 @@
+puts "holamundo"  #  imprimir con salto de linea 
+print "hola mundo"
+print "esto esta muy vacano"  # imprime pantalla 
+
+
+"Y con las palabras "hola mundo"  creo Ruby"
+'vamos pa´ lante"
+
+# caracter de escape \ 
+
